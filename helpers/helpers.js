28f936b919e3beef771e643e7d2a872671147577
Cheza Dzabala <dzabalamacheza@gmail.com>
2019-08-02 13:56:41 +0200

    Revert "Merge pull request #9 from Cheza-Dzabala/ft-create-trips-feature-167594397"
    This reverts commit 35e8035699f857050d196dc4345bd42af0605cdd, reversing
    changes made to 3f61eb187b43b496b35799ae774d05ad69f509e8.

--- helpers/helpers.js
+++
@@ -1,9 +0,0 @@
-
-const generateId = (array) => {
-  const id = array.length + 1;
-  return id;
-};
-
-module.exports = {
-  generateId,
-};
--- server.js
+++
@@ -1,6 +0,0 @@
-import app from './app';
-
-
-const { PORT = 3000 } = process.env;
-
-app.listen(PORT, () => console.log(`listening on port ${PORT}.....`));
